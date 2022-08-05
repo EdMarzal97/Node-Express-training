61ca0f5ab2b07061c70888e7262b547f6e9c9ca7
Edward Marzal <edsanchezmarzal@gmail.com>
2022-08-05 15:04:56 -0500

    dirname

--- myApp.js
+++ myApp.js
@@ -2,11 +2,19 @@ let express = require('express');
 let app = express();
 
 
-console.log("Hello World");
+// console.log("Hello World");
+
+//app.get("/", (req, res) => {
+//    res.send("Hello Express");
+//})
+
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 })
+
+
+
 
 
 
